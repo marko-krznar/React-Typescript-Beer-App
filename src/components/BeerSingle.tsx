051941a8ec2b1557b9fdd79c6cd64bdec10c7793
Marko-batman <krznarm26@gmail.com>
2022-04-20 22:29:41 +0200

    Created Add to favourites functionality

--- src/components/BeerSingle.tsx
+++ src/components/BeerSingle.tsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
+interface IState {
+  favouriteBeers: [];
+}
+
 export default function BeerSingle({ beer }: any) {
+  const [state, setState] = useState<IState>({
+    favouriteBeers: [],
+  });
+  const { favouriteBeers } = state;
+  const handleFavourites = (e: any, beer: any) => {
+    setState({
+      ...favouriteBeers,
+      favouriteBeers: beer,
+    });
+  };
+
   return (
     <div
       className="single-item"
@@ -15,6 +31,9 @@ export default function BeerSingle({ beer }: any) {
       </div>
       <h3>{beer.name}</h3>
       <p>{beer.description}</p>
+      <button onClick={(e) => handleFavourites(e, beer)}>
+        Add to favourites
+      </button>
       <Link to={`/beer/${beer.id}`}>See more</Link>
     </div>
   );
