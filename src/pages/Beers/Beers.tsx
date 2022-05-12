91c11821f9f5f7715a5f3bde70c9532e88e727c7
Marko-batman <krznarm26@gmail.com>
2022-05-12 23:30:59 +0200

    Functionality for favourites

--- src/pages/Beers/Beers.tsx
+++ src/pages/Beers/Beers.tsx
@@ -19,6 +19,8 @@ function Beers() {
 
   const [term, setTerm] = useState("");
   const [alcoholContent, setAlcoholContent] = useState("100");
+  const [favourites, setFavourites] = useState(false);
+  const [favouritebeers, setFavouritebeers] = useState<any[]>([]);
   const { loading, beers, errorMsg } = state;
 
   useEffect(() => {
@@ -46,11 +48,38 @@ function Beers() {
   const filterBeer = beers.filter((beer) => {
     if (
       beer.name.toLowerCase().includes(term) &&
-      beer.abv <= parseFloat(alcoholContent)
+      beer.abv <= parseFloat(alcoholContent) &&
+      // ako je state true prikaži odabrane
+      // ako id piva iz liste favourites ima u beers
+      favourites === false
     ) {
       return beer;
     }
   });
+
+  const handleFavourite = (beer: any) => {
+    const existingBeer = favouritebeers.find((x) => x.id === beer.id);
+    if (existingBeer) {
+      setFavouritebeers(favouritebeers.filter((x) => x.id !== beer.id));
+    } else {
+      setFavouritebeers([...favouritebeers, beer]);
+    }
+  };
+
+  useEffect(() => {
+    console.log(favouritebeers);
+
+    localStorage.setItem("favouriteBeers", JSON.stringify(favouritebeers));
+  }, [favouritebeers]);
+
+  /*
+  PLAN
+  1. spremiti favorite u local session - done!
+  2. što ako se isto pivo spremi dvaput-a? prije spremanja napraviti provjeru i vidjeti dal se pivo nalazi u local session-u.Ako da onda se pivo miće, ako ne onda se dodaje - recimo kao toggle funkcija... - done
+  2.1. Otkrijti zašto se javlja error u funkciji setFavourites, funkcionira true i false - done
+  3. napraviti funkciju koja će usporediti local session i listu piva te prikazati samo piva koja su u local session-u
+  3.1. kako ukomponirati funkcionalnost u već napravljene funkcionalnosti
+  */
 
   return (
     <div className="block--container pg-beers">
@@ -81,13 +110,29 @@ function Beers() {
             <span>100%</span>
           </div>
         </div>
+        <div className="block--favourites">
+          <h3>Show only favourites</h3>
+          <input
+            type="checkbox"
+            id="favourites"
+            name="favourites"
+            value="favourites"
+            onChange={() => setFavourites(!favourites)}
+          ></input>
+        </div>
       </div>
       <div className="d-flex flex-wrap block--prod-list">
         {loading === true && "Loading"}
         {filterBeer.length === 0 && loading === false
           ? `There are no beers with filters you provided`
           : filterBeer.map((beer) => {
-              return <BeerSingle key={beer.id} beer={beer} />;
+              return (
+                <BeerSingle
+                  key={beer.id}
+                  beer={beer}
+                  handleFavourite={handleFavourite}
+                />
+              );
             })}
         {beers.length === 0 && errorMsg}
       </div>
