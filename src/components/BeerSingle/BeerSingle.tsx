f26c2d51ed406857d112fb4f7a53615a00c0cdf2
Marko-batman <krznarm26@gmail.com>
2022-05-15 20:45:27 +0200

    Removed code for feature favourite beers

--- src/components/BeerSingle/BeerSingle.tsx
+++ src/components/BeerSingle/BeerSingle.tsx
@@ -1,23 +1,7 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./style.scss";
 
-interface IState {
-  favouriteBeers: [];
-}
-
 export default function BeerSingle({ beer }: any) {
-  const [state, setState] = useState<IState>({
-    favouriteBeers: [],
-  });
-  const { favouriteBeers } = state;
-  const handleFavourites = (e: any, beer: any) => {
-    setState({
-      ...favouriteBeers,
-      favouriteBeers: beer,
-    });
-  };
-
   return (
     <div className="single-item">
       <span className="block--abv">{beer.abv}%</span>
