0d8cb6747a988ba44b3427ee18b76b54e577c6d8
marko <krznarm26@gmail.com>
2023-07-11 21:06:45 +0200

    Replaced logo with icon

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,18 +1,18 @@
 import { Link } from "react-router-dom";
 import "./style.scss";
-import logo from "../../images/beer-logo.webp";
+
+import { BiSolidBeer } from "react-icons/bi";
 
 export default function Navigation() {
-  return (
-    <div className="block--container nav d-flex justify-content-between align-items-center m--flex-direction-column">
-      {" "}
-      <div className="block-logo">
-        <img src={logo} alt="beer-logo" />
-      </div>
-      <div className="block--links">
-        <Link to={"/React-Typescript-Beer-App"}>Home</Link>
-        <Link to={"/React-Typescript-Beer-App/beers"}>Beers</Link>
-      </div>
-    </div>
-  );
+	return (
+		<div className="block--container nav d-flex justify-content-between align-items-center m--flex-direction-column">
+			<div className="block-logo">
+				<BiSolidBeer className="icon-logo" />
+			</div>
+			<div className="block--links">
+				<Link to={"/React-Typescript-Beer-App"}>Home</Link>
+				<Link to={"/React-Typescript-Beer-App/beers"}>Beers</Link>
+			</div>
+		</div>
+	);
 }
