df26f8bc3ee5086c6b6d41c9b23eed05dd332419
marko <krznarm26@gmail.com>
2023-08-20 15:54:14 +0200

    Added new UI for sindle Beer page

--- src/pages/SingleBeer/SingleBeer.tsx
+++ src/pages/SingleBeer/SingleBeer.tsx
@@ -5,91 +5,100 @@ import { BeersService } from "../../services/BeersService";
 import "./style.scss";
 
 interface URLParams {
-  id: string;
+	id: string;
 }
 
 interface IState {
-  singleBeer: ISingleBeer;
-  errorMsg: string;
+	singleBeer: ISingleBeer;
+	errorMsg: string;
 }
 
 function SingleBeer() {
-  const { id } = useParams<URLParams | any>();
+	const { id } = useParams<URLParams | any>();
 
-  const [beer, setBeer] = useState<IState>({
-    singleBeer: {} as ISingleBeer,
-    errorMsg: "",
-  });
+	const [beer, setBeer] = useState<IState>({
+		singleBeer: {} as ISingleBeer,
+		errorMsg: "",
+	});
 
-  useEffect(() => {
-    if (id) {
-      BeersService.getSingleBeer(id)
-        .then((res) => {
-          setBeer({
-            ...beer,
-            singleBeer: res.data,
-          });
-        })
-        .catch((err) => {
-          setBeer({
-            ...beer,
-            errorMsg: err.message,
-          });
-        });
-    }
-  }, [id]);
+	useEffect(() => {
+		if (id) {
+			BeersService.getSingleBeer(id)
+				.then((res) => {
+					setBeer({
+						...beer,
+						singleBeer: res.data,
+					});
+				})
+				.catch((err) => {
+					setBeer({
+						...beer,
+						errorMsg: err.message,
+					});
+				});
+		}
+	}, [id]);
 
-  let { singleBeer } = beer;
+	let { singleBeer } = beer;
 
-  return (
-    <div className="block--container pg--single-product">
-      {singleBeer != null && Array.isArray(singleBeer) && (
-        <>
-          <div className="block--beer-info d-flex justify-content-center align-items-center">
-            <div className="block--image-frame">
-              <img src={singleBeer[0].image_url} alt={singleBeer[0].name} />
-            </div>
-            <div className="block--info">
-              <div className="block--headline">
-                <h2>{singleBeer[0].name}</h2>
-                <span className="txt-tag">#{singleBeer[0].tagline}</span>
-                <p>{singleBeer[0].description}</p>
-              </div>
-              <div className="block--specs d-flex m--flex-direction-column">
-                <div>
-                  <h3>
-                    <i className="bi bi-hand-thumbs-up-fill"></i> Food pairing
-                  </h3>
-                  <ul>
-                    {singleBeer[0].food_pairing.map(
-                      (food: string, index: number) => {
-                        return <li key={index}>{food}</li>;
-                      }
-                    )}
-                  </ul>
-                </div>
-                <div>
-                  <h3>
-                    <i className="bi bi-hand-index-fill"></i> Brewers tips
-                  </h3>
-                  <p>{singleBeer[0].brewers_tips}</p>
-                </div>
-              </div>
-              <div className="block--bcg-btn">
-                <Link
-                  className="tag--back"
-                  to={"/React-Typescript-Beer-App/beers"}
-                >
-                  <i className="bi bi-chevron-left"></i>
-                  <span>Back to Beers</span>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </>
-      )}
-    </div>
-  );
+	return (
+		<div className="block--container pg--single-product">
+			{singleBeer != null && Array.isArray(singleBeer) && (
+				<>
+					<div className="block--beer-info d-flex justify-content-center">
+						<div className="block--image-frame">
+							<img
+								src={singleBeer[0].image_url}
+								alt={singleBeer[0].name}
+							/>
+						</div>
+						<div className="block--info">
+							<div className="block--headline">
+								<span className="txt-tag">
+									{singleBeer[0].tagline}
+								</span>
+								<h1>{singleBeer[0].name}</h1>
+								<p>{singleBeer[0].description}</p>
+							</div>
+							<div className="block--specs d-flex m--flex-direction-column">
+								<div>
+									<h2>
+										<i className="bi bi-hand-thumbs-up-fill"></i>{" "}
+										Food pairing
+									</h2>
+									<ul>
+										{singleBeer[0].food_pairing.map(
+											(food: string, index: number) => {
+												return (
+													<li key={index}>{food}</li>
+												);
+											}
+										)}
+									</ul>
+								</div>
+								<div>
+									<h2>
+										<i className="bi bi-hand-index-fill"></i>{" "}
+										Brewers tips
+									</h2>
+									<p>{singleBeer[0].brewers_tips}</p>
+								</div>
+							</div>
+							{/* <div className="block--bcg-btn"> */}
+							<Link
+								className="tag--back btn"
+								to={"/React-Typescript-Beer-App/beers"}
+							>
+								{/* <i className="bi bi-chevron-left"></i> */}
+								Back to Beers
+							</Link>
+							{/* </div> */}
+						</div>
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
 
 export default SingleBeer;
